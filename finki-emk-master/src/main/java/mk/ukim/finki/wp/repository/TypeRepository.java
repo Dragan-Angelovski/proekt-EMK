7585e142eab20b44fb4d96370c70b2ce0e7d71b4
AleksandraBazgovska <aleksandrabazgovska@gmail.com>
2015-05-12 14:52:37 +0200

    commit conflicti

--- finki-emk-master/src/main/java/mk/ukim/finki/wp/repository/TypeRepository.java
+++ finki-emk-master/src/main/java/mk/ukim/finki/wp/repository/TypeRepository.java
@@ -2,7 +2,6 @@ package mk.ukim.finki.wp.repository;
 
 import java.util.List;
 
-
 import mk.ukim.finki.wp.model.TypeProduct;
 
 public interface TypeRepository extends JpaSpecificationRepository<TypeProduct> {
