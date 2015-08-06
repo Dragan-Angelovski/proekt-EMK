a8e0003954620d478b2afd2db58125621f98822d
Martin123VP <mart1nc0@hotmail.com>
2015-08-06 15:44:22 +0200

    123

--- finki-emk-master/src/main/java/mk/ukim/finki/wp/model/Stall.java
+++ finki-emk-master/src/main/java/mk/ukim/finki/wp/model/Stall.java
@@ -1,16 +1,23 @@
 package mk.ukim.finki.wp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
 
 @Entity
 @Table(name = "stalls")//tezga
+@Indexed
 public class Stall extends BaseEntity{
 
-		
+	@Column(unique = true, nullable = false)
+    @Field(index = Index.YES, analyze = Analyze.YES, store = Store.NO)
 	private int number;
 	
 	@ManyToOne
